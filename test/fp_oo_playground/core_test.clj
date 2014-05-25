2d5124565e5000b610ad933a1db2f4b2edfac1d4
David G Chaves <davidgchaves@gmail.com>
2014-05-25 17:21:26 +0200

    Delete useless leiningen generated files

--- test/fp_oo_playground/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns fp-oo-playground.core-test
-  (:require [clojure.test :refer :all]
-            [fp-oo-playground.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
