2d35683b49f24b32c6f17140c0a3aaa48af6f173
David G Chaves <davidgchaves@gmail.com>
2014-05-26 18:57:35 +0200

    Point class with encapsulated getters and add

--- src/fp_oo_playground/04_embedded_functions.clj
+++ src/fp_oo_playground/04_embedded_functions.clj
@@ -6,7 +6,7 @@
 
 (def send-to
   (fn [object message & args]
-    (apply (message (:__methods__ object) object args))))
+    (apply (message (:__methods__ object)) object args)))
 
 (def Point
   (fn [x y]
@@ -15,7 +15,20 @@
      :__class_symbol__ 'Point
      :__methods__ {
                    :class :__class_symbol__
-                   :shift
-                   (fn [this x-inc y-inc]
-                     (make Point (+ (:x this) x-inc)
-                                 (+ (:y this) y-inc)))}}))
+                   :x :x
+                   :y :y
+                   :shift (fn [this x-inc y-inc]
+                            (make Point (+ (:x this) x-inc)
+                                        (+ (:y this) y-inc)))
+                   :add (fn [this that]
+                          (send-to this
+                                   :shift
+                                   (send-to that :x) (send-to that :y)))}}))
+
+(def point-a (make Point 10 20))
+(def point-b (make Point 50 30))
+
+(send-to point-a :x)
+(send-to point-a :y)
+(send-to point-a :shift 100 1000)
+(send-to point-a :add point-b)
