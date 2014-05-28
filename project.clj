18ab53b7a5ea43caf2e2aa55f49c7d78946c614d
David G Chaves <davidgchaves@gmail.com>
2014-05-28 17:15:48 +0200

    Move the class out of the constructor

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :repl-options {:init-ns fp-oo-playground.04-embedded-functions})
+  :repl-options {:init-ns fp-oo-playground.05-class})
---
+++ src/fp_oo_playground/05_class.clj
@@ -0,0 +1,38 @@
+(ns fp-oo-playground.05-class)
+
+(def explicit-3-step-object-creation-and-initialization-make
+  (fn [class & args]
+    (let [allocated   {}
+          seeded      (assoc allocated :__class_symbol__ (:__own_symbol__ class))
+          constructor (:add-instance-values (:__instance_methods__ class))]
+      (apply constructor seeded args))))
+
+(def make
+  (fn [class & args]
+    (let [seeded      {:__class_symbol__ (:__own_symbol__ class)}
+          constructor (:add-instance-values (:__instance_methods__ class))]
+     (apply constructor seeded args))))
+
+(def send-to
+  (fn [instance message & args]
+    (let [class  (eval (:__class_symbol__ instance))
+          method (message (:__instance_methods__ class))]
+     (apply method instance args))))
+
+(def Point
+  {
+   :__own_symbol__ 'Point
+   :__instance_methods__
+   {
+    :class :__class_symbol__
+
+    :add-instance-values (fn [this x y]
+                           (assoc this :x x :y y))
+
+    :shift (fn [this x-inc y-inc]
+             (make Point (+ (:x this) x-inc)
+                         (+ (:y this) y-inc)))
+    }
+   })
+
+(send-to (make Point 1 2) :shift -2 -3)
